8542aaeb1e37e23cebcba5691b8c4915b083533b
Yan Feng <t34@qq.com>
2014-10-20 19:33:43 +0800

    Add template decorator

--- FlaskWebProject2/FlaskWebProject2/__init__.py
+++ FlaskWebProject2/FlaskWebProject2/__init__.py
@@ -3,9 +3,12 @@
 The flask application package.
 """
 
+from datetime import datetime
 from flask import Flask
+
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'why would I tell you my secret key?'
+app.jinja_env.globals['year'] = datetime.now().year
 
 import FlaskWebProject2.views
--- FlaskWebProject2/FlaskWebProject2/views.py
+++ FlaskWebProject2/FlaskWebProject2/views.py
@@ -3,18 +3,16 @@
 Routes and views for the flask application.
 """
 
-from datetime import datetime
 from flask import render_template, request, redirect, url_for
 from flask.ext.login import login_required, login_user, logout_user, current_user
 
-from FlaskWebProject2 import app
-from FlaskWebProject2 import users
+from FlaskWebProject2 import app, users
+from template_decorator import templated
 from models import *
-
-import jinja2
 
 @app.route('/')
 @app.route('/index')
+@templated('index.html')
 def home():
     """Renders the home page."""
     ts=get_all_table_names()
@@ -23,26 +21,17 @@ def home():
     for item in ts:
         c = get_table_description(item)
         ds.append(c)
-    return render_template(
-        'index.html',
-        title=u'主页',
-        year=datetime.now().year,
-        db_name='SZVTSMIS_SERVER',
-        tbls=zip(ts, ds)
-    )
+    return dict(title=u'主页', db_name='SZVTSMIS_SERVER', tbls=zip(ts, ds))
 
 @app.route('/login', methods=["GET", "POST"])
+@templated('login.html')
 def login():
     if request.method == 'POST' and request.form['user_name'] == 'admin':
         # login and validate the user...
         login_user(users.User())
         #flash("Logged in successfully.")
         return redirect(request.args.get("next") or url_for("home"))
-    return render_template(
-        'login.html',
-        title=u'登录',
-        year=datetime.now().year,
-    )
+    return dict(title=u'登录')
 
 @app.route("/logout")
 @login_required
@@ -51,20 +40,17 @@ def logout():
     return redirect("/")
 
 @app.route('/logs')
+@templated('logs.html')
 def logs():
     """查看Tomcat服务器日志"""
     import os, glob
     root = os.environ['CATALINA_HOME']
     fullpaths = glob.glob("%s\logs\*.log" % root)
     filenames = [os.path.basename(item) for item in fullpaths]
-    return render_template(
-        'logs.html',
-        title=u'日志工具',
-        year=datetime.now().year,
-        log_names = filenames
-    )
+    return dict(title=u'日志工具', log_names = filenames)
 
 @app.route('/logs/<log_name>')
+@templated('logs.html')
 def logs_detail(log_name):
     """查看Tomcat服务器日志"""
     import os
@@ -73,15 +59,10 @@ def logs_detail(log_name):
     f = open(fullpath, 'r')
     t = f.read().decode('gb2312')
     f.close()
-    return render_template(
-        'logs.html',
-        title=u'日志工具',
-        year=datetime.now().year,
-        log_name = log_name,
-        log_content = t
-    )
+    return dict(title=u'日志工具', log_name=log_name,log_content=t)
 
 @app.route('/sql', methods=['GET', 'POST'])
+@templated('sql.html')
 @login_required
 def sql():
     if request.method == 'POST':
@@ -91,23 +72,15 @@ def sql():
             rs = []
             if result.returns_rows:
                 rs = result.fetchall()
-            return render_template(
-                'sql.html',
-                title=u'SQL工具',
-                year=datetime.now().year,
-                previous_sql=query,
-                resultSet=rs,
-                keys=result.keys(),
-                nrows=result.rowcount
-            )
-    return render_template(
-        'sql.html',
-        title=u'SQL工具',
-        year=datetime.now().year,
-        previous_sql=''
-    )
+            return dict(title=u'SQL工具',
+                        previous_sql=query,
+                        resultSet=rs,
+                        keys=result.keys(),
+                        nrows=result.rowcount)
+    return dict(title=u'SQL工具',previous_sql='')
 
 @app.route('/db/<database>')
+@templated('database.html')
 def database(database):
     ts=get_all_table_names()
     ts.sort()
@@ -115,25 +88,18 @@ def database(database):
     for item in ts:
         c = get_table_description(item)
         ds.append(c)
-    return render_template(
-        'database.html',
-        title=u'数据库',
-        year=datetime.now().year,
-        db_name=database,
-        tbls=zip(ts, ds),
-    )
+    return dict(title=u'数据库', db_name=database, tbls=zip(ts, ds))
 
 @app.route('/db/<database>/<table>')
+@templated('table.html')
 def table(database, table):
     descr = get_table_description(table)
     result = execute_raw_sql2("select * from %s" % table)
     rs = []
     if result.returns_rows:
         rs = result.fetchall()
-    return render_template(
-        'table.html',
+    return dict(
         title=u'数据表',
-        year=datetime.now().year,
         db=database,
         tbl=table,
         resultSet=rs,
@@ -145,21 +111,19 @@ def table(database, table):
     )
 
 @app.route('/contact')
+@templated('contact.html')
 def contact():
     """Renders the contact page."""
-    return render_template(
-        'contact.html',
+    return dict(
         title='Contact',
-        year=datetime.now().year,
         message='Your contact page.'
     )
 
 @app.route('/about')
+@templated('about.html')
 def about():
     """Renders the about page."""
-    return render_template(
-        'about.html',
+    return dict(
         title='About',
-        year=datetime.now().year,
         message='Your application description page.'
     )
